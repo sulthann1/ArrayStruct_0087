2917d78a49aeb66b653b54a10617587f4e0d0a48
sulthann1 <sulthanaawaliya155@gmail.com>
2025-04-15 16:48:28 +0700

    struct mahasiswa nama nim alamat

--- struct.cpp
+++ struct.cpp
@@ -6,3 +6,19 @@ struct mahasiswa{
     string nama;
     string alamat;
 };
+
+int main(){
+    mahasiswa mhs;
+
+    mhs.nim ="20240140001";
+    mhs.nama ="Abra";
+    mhs.alamat ="Kasihan";
+    
+    cout << "Masukan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukan Nama = ";
+    cin >> mhs.nama;
+    cout << "Masukan Alamat =";
+    cin >> mhs.alamat;
+
+}
