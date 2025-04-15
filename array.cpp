3cc7246781ec5214cb652f90f60a9951db86c8fa
sulthann1 <sulthanaawaliya155@gmail.com>
2025-04-15 16:42:27 +0700

    mengisi array pada lokasi/index tertentu

--- array.cpp
+++ array.cpp
@@ -7,3 +7,15 @@ int nilai[] = {2,5,9,10,34};
 int skor [5] = {8,12,18,6};
 //deklarasi array dengan ukuran dan tidak ada nilai awal
 string nama[3];
+
+int main(){
+    cout <<"Skor ke 3 =" << skor[2]<<endl;
+    //mengisi array pada lokasi/index tertentu
+    skor [2] = 25;
+    cout <<"Skor ke 3 =" << skor [2] << endl;
+       //mengisi array dengan loop for
+   for (int i=0; i< 3; i++){
+    cout << "Nama Mahasiswa ke-" << i+1 << "=";
+    cin >> nama [i];
+   } 
+}
