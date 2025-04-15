e895d4102448e49704830f5dd4998bd9db8b52f9
sulthann1 <sulthanaawaliya155@gmail.com>
2025-04-15 16:38:45 +0700

    deklarasi array tanpa ukuran, tetapi wajib mengisikan data

---
+++ array.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+//deklarasi array tanpa ukuran, tetapi wajib mengisikan data
+int nilai[] = {2,5,9,10,34};
+//deklarasi array dengan ukuran dan diberi nilai awal
+int skor [5] = {8,12,18,6};
+//deklarasi array dengan ukuran dan tidak ada nilai awal
+string nama[3];
