e74d53439753c29ae0ce3475c294702a51ca29d0
sulthann1 <sulthanaawaliya155@gmail.com>
2025-04-15 16:50:18 +0700

    struct mahasiswa dengan alamat

---
+++ struct2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa{
+    string nim;
+    string nama;
+    string alamat;
+};
+
+int main(){
+    mahasiswa mhs;
+
+    mhs.nim ="20240140001";
+    mhs.nama ="Abra";
+    mhs.alamat ="Kasihan";
+    
