787818d9c71c78f932ca4cbd8eb14257fdc3a01c
sulthann1 <sulthanaawaliya155@gmail.com>
2025-04-15 16:53:08 +0700

    mengignore cin agar bisa lebih dua kata

--- struct3.cpp
+++ struct3.cpp
@@ -4,3 +4,34 @@ using namespace std;
 struct detailAlamat{
     string desa;
     string kota;
+};
+
+struct mahasiswa{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+};
+
+int main(){
+    mahasiswa mhs;
+    
+    cout << "Masukan NIM = ";
+    cin >> mhs.nim;
+    cin.ignore();
+    cout << "Masukan Nama = ";
+    getline (cin, mhs.nama);
+    cin >> mhs.nama;
+    cout << "Masukan Alamat Desa =";
+    cin >> mhs.alamat.desa;
+    cout << "Masukan Alamat Kota =";
+    cin >> mhs.alamat.kota;
+
+    cout << endl;
+    cout << "Data Mahasiswa" << endl;
+    
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Alamat Desa = " << mhs.alamat.desa << endl;
+    cout << "Alamat Kota = " << mhs.alamat.kota << endl;
+
+}
