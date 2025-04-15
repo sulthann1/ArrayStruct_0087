0355b317b7babbc80d169d1fa2395744d9f0fefb
sulthann1 <sulthanaawaliya155@gmail.com>
2025-04-15 16:50:58 +0700

    output input struct mahasiswa

--- struct2.cpp
+++ struct2.cpp
@@ -13,4 +13,12 @@ int main(){
     mhs.nim ="20240140001";
     mhs.nama ="Abra";
     mhs.alamat ="Kasihan";
-    
+
+    cout << "Masukan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukan Nama = ";
+    cin >> mhs.nama;
+    cout << "Masukan Alamat =";
+    cin >> mhs.alamat;
+
+}
